76b4017c5539c3f13af05c6fe80e951b7fe389d4
Firzus <lprieu@gaming.tech>
2024-01-15 21:47:14 +0100

    ignore & remove dist folder

--- dist/_next/static/chunks/app/layout-55d185c445ba5595.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7205:function(n,e,t){Promise.resolve().then(t.t.bind(t,1934,23)),Promise.resolve().then(t.t.bind(t,7456,23))},7456:function(){},1934:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=7205)}),_N_E=n.O()}]);
