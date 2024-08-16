3a615ce20c6aed7213586af9dc985b73ec7345a5
Firzus <92395888+Firzus@users.noreply.github.com>
2024-08-16 15:51:44 +0200

    test pr

--- components/canvas/Earth.jsx
+++ components/canvas/Earth.jsx
@@ -1,23 +1,18 @@
-'use client'
+"use client";
 
-import { Canvas } from "@react-three/fiber"
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from '../Loader'
-import { Suspense } from "react"
+import CanvasLoader from "../Loader";
+import { Suspense } from "react";
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive
-      object={earth.scene}
-      scale={2.5}
-      position-y={0}
-      rotation-y={0}
-    />
-  )
-}
+    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+  );
+};
 
 const EarthCanvas = () => {
   return (
@@ -29,7 +24,7 @@ const EarthCanvas = () => {
         fov: 45,
         near: 0.1,
         far: 200,
-        position: [-4, 3, 6]
+        position: [-4, 3, 6],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -43,7 +38,8 @@ const EarthCanvas = () => {
         <Earth />
       </Suspense>
     </Canvas>
-  )
-}
+  );
+};
 
-export default EarthCanvas
+export default EarthCanvas;
+//
