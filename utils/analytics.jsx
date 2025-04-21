11a39869f83cc1246ec4559501da0396d6e32ff3
Firzus <92395888+Firzus@users.noreply.github.com>
2025-04-21 03:35:00 +0200

    chore: update dependencies and integrate Ackee analytics
    - Updated Next.js version from 14.0.4 to ^14.2.28 in package.json.
    - Added ackee-tracker dependency version ^5.1.0 in package.json.
    - Modified analytics.jsx to use Ackee for tracking:
      - Changed the analytics script source to a new URL.
      - Added data attributes for Ackee server and domain ID.
      - Set script loading strategy to lazyOnload.

--- utils/analytics.jsx
+++ utils/analytics.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Script from "next/script";
 
 export default function Analytics() {
@@ -7,9 +9,10 @@ export default function Analytics() {
 
   return (
     <Script
-      defer
-      data-domain="portfolio.lprieu.dev"
-      src="https://analytics.lprieu.dev/js/script.js"
+      src="https://lprieu-analytics.up.railway.app/p12gy4mlfptmuc6w.js"
+      data-ackee-server="https://lprieu-analytics.up.railway.app"
+      data-ackee-domain-id="2fcd2124-0135-4012-8a06-61b9daf10f03"
+      strategy="lazyOnload"
     />
   );
 }
