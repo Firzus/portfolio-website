67d884f14509ee93d1a5703732e08a953d67bd3b
Firzus <92395888+Firzus@users.noreply.github.com>
2025-04-21 03:43:30 +0200

    fix: add missing data-ackee-opts attribute to analytics script

--- utils/analytics.jsx
+++ utils/analytics.jsx
@@ -12,6 +12,7 @@ export default function Analytics() {
       src="https://lprieu-analytics.up.railway.app/p12gy4mlfptmuc6w.js"
       data-ackee-server="https://lprieu-analytics.up.railway.app"
       data-ackee-domain-id="2fcd2124-0135-4012-8a06-61b9daf10f03"
+      data-ackee-opts='{ "detailed": true, "ignoreOwnVisits": true }'
       strategy="lazyOnload"
     />
   );
